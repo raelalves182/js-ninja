7039e4f1e178fe5b631356f45ada9e0d868582fd
raelalves182 <ysrael.2009@hotmail.com>
2021-11-25 21:10:16 -0300

    feat(header): use strict

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 (function() {
-  console.log('Js no Browser'); 
+  // escopo local se não usar o 'var' ele cria a variável em escopo global também
+  // 'use strict' deixa em escopo restrito
+  var name = 'Israel Alves'
+  console.log(name); 
 })();
