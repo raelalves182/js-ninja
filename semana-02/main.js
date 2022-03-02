b6d95c069937d0101448708da8151a716a23d0a8
Israel Alves <ysrael.2009@hotmail.com>
2022-03-01 23:47:36 -0300

    feat(header): js

--- semana-02/main.js
+++ semana-02/main.js
@@ -74,6 +74,9 @@
   5. Se nenhum argumento for passado, retorne o valor booleano `false`.
   6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
   */
+ function Core(a, b, c) {
+   
+ }
 
 
   // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
