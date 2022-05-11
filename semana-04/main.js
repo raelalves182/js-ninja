ce5254cad31e0c722940a1841954deeaf8f01eb4
Israel Alves <ysrael.2009@hotmail.com>
2022-05-10 21:43:22 -0300

    feat(header): desafio 4

--- semana-04/main.js
+++ semana-04/main.js
@@ -8,15 +8,28 @@ um único parâmetro como argumento. Essa função deve retornar `true` se o
 equivalente booleano para o valor passado no argumento for `true`, ou `false`
 para o contrário.
 */
-
+  var isTruthy = function(param) {
+    return !!param;
+  };
  
  // Invoque a função criada acima, passando todos os tipos de valores `falsy`.
-
+  isTruthy(false);
+  isTruthy(null);
+  isTruthy(undefined);
+  isTruthy('');
+  isTruthy(0);
+  isTruthy(NaN);
  
  /*
  Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
  */
-
+  isTruthy(true);
+  isTruthy(' ');
+  isTruthy(8);
+  isTruthy('Rael');
+  isTruthy([]);
+  isTruthy({});
+  isTruthy(function() {});
  
  /*
  Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
