8e5e68891adfcdc8c098f688c29ee2876eacc3c0
Israel Alves <ysrael.2009@hotmail.com>
2022-05-11 21:24:49 -0300

    feat(header): resolve questions

--- semana-04/main.js
+++ semana-04/main.js
@@ -43,35 +43,54 @@ para o contrário.
  - `assentos` - Number - cinco por padrão
  - `quantidadePessoas` - Number - zero por padrão
  */
-
+  var carro = {
+    marca: 'Polo',
+    modelo: 'Volkswagen',
+    placa: 'QLE7228',
+    ano: 2022,
+    cor: 'Branco',
+    quantasPortas: 4,
+    assentos: 5,
+    quantidadePessoas: 0
+  }
  
  /*
  Crie um método chamado `mudarCor` que mude a cor do carro conforme a cor
  passado por parâmetro.
  */
-
+  carro.mudarCor = function( cor ) {
+    carro.cor = cor;
+  };
  
  /*
  Crie um método chamado `obterCor`, que retorne a cor do carro.
  */
-
+  carro.obterCor = function() {
+    return carro.cor;
+  };
  
  /*
  Crie um método chamado `obterModelo` que retorne o modelo do carro.
  */
-
+  carro.obterModelo = function() {
+    return carro.modelo;
+  };
 
  /*
  Crie um método chamado `obterMarca` que retorne a marca do carro.
  */
-
+  carro.obterMarca = function() {
+    return carro.marca;
+  };
  
  /*
  Crie um método chamado `obterMarcaModelo`, que retorne:
  "Esse carro é um [MARCA] [MODELO]"
  Para retornar os valores de marca e modelo, utilize os métodos criados.
  */
-
+  carro.obterMarcaModelo = function() {
+    return 'Esse carro é um ' + carro.obterMarca() + ' ' + carro.obterModelo();
+  };
  
  /*
  Crie um método que irá adicionar pessoas no carro. Esse método terá as
