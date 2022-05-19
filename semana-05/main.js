430da76794e6c2583fb7f68f2b4e6847a7c1cf1c
Israel Alves <ysrael.2009@hotmail.com>
2022-05-18 23:28:51 -0300

    feat(header): question 3 of 11

--- semana-05/main.js
+++ semana-05/main.js
@@ -6,17 +6,19 @@
   Crie uma variável qualquer, que receba um array com alguns valores aleatórios
   - ao menos 5 - (fica por sua conta os valores do array).
   */
-  // ?
+  var rael = [ 'EL', 182, true, null, 'blink' ];
 
   /*
   Crie uma função que receba um array como parâmetro, e retorne esse array.
   */
-  // ?
+  function myFunction(arr) {
+    return arr;
+  }
 
   /*
   Imprima o segundo índice do array retornado pela função criada acima.
   */
-  // ?
+  myFunction(rael)[1];
 
   /*
   Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
